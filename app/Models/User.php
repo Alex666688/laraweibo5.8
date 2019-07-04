847eb698d1d3645f3edb382d1f683cc9b70b1e29
alex <1414818093@qq.com>
2019-07-04 13:14:02 +0800

    用户个人信息显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,23 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    /**
+     * 生成在线头像
+     *
+     * @param string $size
+     * @return string
+     */
+    public function gravatar($size = '100')
+    {
+        // $this->attributes['email']  可以直接获取用户邮箱
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
+
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,23 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
 
 
+// 显示所有用户列表的页面
+Route::get('/users', 'UsersController@index')->name('users.index');
+
+// 创建用户的页面
+Route::get('/users/create', 'UsersController@create')->name('users.create');
+
+// 显示用户个人信息的页面
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+
+// 创建用户
+Route::post('/users', 'UsersController@store')->name('users.store');
+
+// 编辑用户个人资料的页面
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+
+// 更新用户
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+
+// 删除用户
+Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
