8477e9d6b9359244e6eae58806886d905f5ff7fe
alex <1414818093@qq.com>
2019-07-04 15:24:07 +0800

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,14 +9,38 @@ class UsersController extends Controller
 {
 
 
+    /**
+     * 用户注册显示页面
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function create ()
     {
         return view('users.create');
     }
 
+    /**
+     * 个人信息显示页面
+     *
+     * @param User $user
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(User $user)
     {
         return view('users.show', compact('user'));
     }
+
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
+
+
 
 }
