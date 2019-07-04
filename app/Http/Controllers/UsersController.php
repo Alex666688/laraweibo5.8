048376f3a961ab9ddfcb222f562c3ad6b9a8a2fa
alex <1414818093@qq.com>
2019-07-04 09:37:09 +0800

    用户注册页面

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+
+
+    public function create ()
+    {
+        dump(5555);die;
+        return view('users.create');
+    }
+
+
+}
